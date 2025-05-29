ca3f87bf95081094ff36c2e0ed714b6b272d0cb5
bharath-rjendran <135965157+bharath-rjendran@users.noreply.github.com>
2025-05-30 00:21:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "var.subscription_id"
-  client_id       = "var.client_id"
-  tenant_id       = "var.tenant_id"
-  client_secret   = "var.client_secret"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
