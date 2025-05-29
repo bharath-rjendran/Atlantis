d3d2c42545b00b1c074a889b04949284687ed4d7
bharath-rjendran <135965157+bharath-rjendran@users.noreply.github.com>
2025-05-29 22:51:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,16 @@ resource "azurerm_virtual_network" "vnet" {
   }
 }
 
+resource "azurerm_subnet" "subnet" {
+  count             = length(var.subnet_names)
+  name                 = var.subnet_names[count.index]
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_cidr_blocks[count.index]]
+
+  depends_on = [azurerm_virtual_network.vnet]
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   location            = var.location
