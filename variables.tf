9dab62dda91a8c0320fa30693fc3be4aba12c072
bharath-rjendran <135965157+bharath-rjendran@users.noreply.github.com>
2025-05-29 22:51:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+}
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create resources"
+  type        = string
+}
+variable "address_space" {
+  description = "The address space for the virtual network"
+  type        = list(string)
+}
+variable "subnet_names" {
+  description = "A map of subnets to create in the virtual network"
+  type        = list(string)
+}
+variable "environment" {
+  description = "The environment for the networking resources (e.g., dev, staging, prod)"
+  type        = string
+}
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+}
+variable "admin_username" {
+  description = "The username for the virtual machine"
+  type        = string
+}
+variable "admin_password" {
+  description = "The password for the virtual machine"
+  type        = string
+}
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+variable "subnet_name" {
+  description = "The name of the subnet in which to create the virtual machine"
+  type        = string
+}
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+variable "subnet_id" {
+    description = "The ID of the subnet in which to create the virtual machine"
+    type        = string
+}
+
